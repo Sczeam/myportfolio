3954878fd74031b2a31541482613a4aa281eb2cf
Sczeam <zawhtikeaung19@gmail.com>
2026-03-20 13:21:28 +0000

    Build selected work intro section

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import { Hero } from "@/components/home/hero";
 import { OpeningSequenceMotion } from "@/components/home/opening-sequence-motion";
+import { SelectedWorkIntro } from "@/components/home/selected-work-intro";
+import { SelectedWorkIntroMotion } from "@/components/home/selected-work-intro-motion";
 import { ThesisSection } from "@/components/home/thesis-section";
 
 export default function Home() {
   return (
     <main className="flex-1">
       <OpeningSequenceMotion />
+      <SelectedWorkIntroMotion />
       <Hero />
       <ThesisSection />
+      <SelectedWorkIntro />
     </main>
   );
 }
